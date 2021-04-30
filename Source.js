121602bd8abd2682639f7542f53fb24a938c291f
(i) <81922230+numexp@users.noreply.github.com>
2021-04-29 22:10:09 -0400

    Create Source.js

---
+++ Source.js
@@ -0,0 +1,53 @@
+// We import fs for our file handling.
+const Task = require('fs');
+
+// Clear console+display copyright.
+console.clear();
+console.log("\n \n [i] => Zoom Code Generator By ro0ti \n \n");
+
+// Timer that ticks per 1000ms = 1 second.
+var interval = setInterval (function ()
+{
+	runSniper();
+}, 1000);
+
+// Writes out log file, ouputs an error if there is one.
+function writeLog(data) {
+    Task.appendFile('log.txt', data, function (error) {
+        if (error) {
+            console.log('[ERROR] => ' + error);
+        } else {
+            console.log('[ENTRIES] => ' + ID);
+        }
+    });
+}
+
+// Global ID for entries created on session.
+var ID = 0;
+
+// Formats everything per tick.
+function runSniper() {
+    var Code_1 = GenerateCode(3);
+    var Code_2 = GenerateCode(3);
+    var Code_3 = GenerateCode(4);
+    var Code_Combined = Code_1 + Code_2 + Code_3;
+    var ZoomLink = 'https://zoom.us/j/';
+    var ZoomFormat = Code_1 + '-' + Code_2 + '-' + Code_3;
+    if (ID == 0) {
+        ID = 1;
+    } else {
+        ID = ID + 1;
+    }
+    writeLog(ZoomLink + Code_Combined + '\n' + ZoomFormat + '\n \n');
+    // Generates each code per tick.
+    function GenerateCode(length) {
+        var result           = '';
+        var characters       = '0123456789';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
+}
+// Created By ro0ti
